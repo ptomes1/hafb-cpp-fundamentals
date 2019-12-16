08bef8a711d697d33305af8c611d6f5ef807b9ce
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:04 -0700

    Add cin and an name space

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-std :: cout << "Hello World" << std :: endl;
-return 0;
+    int age; //int: signed integers
+    //cout: for system output
+    std :: cout << "Hello World" << std :: endl;
+    cout << "Another line under namespace std" << endl;
+    cout << "Wildcats " << "WSU " << "Ogden" << endl;
+    cout << "What is your age" << endl;
+    // cin: for system/user input
+    cin >> age;
+    cout << "I see you are : "<<age << endl;
+
+    return 0;
 }

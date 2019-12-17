c855696e3d3e1a2d6c9212b9ada7f107df17b0e9
uradmin <uradmin@ad.weber.edu>
2019-12-17 11:59:56 -0700

    Max

--- Module1/LA1-3/my_functions.cpp
+++ Module1/LA1-3/my_functions.cpp
@@ -17,3 +17,44 @@ void UpdateStep(int& step)
     step +=1;
     cout << "New Step : " << step << endl;
 }
+
+int Max(int num, int num2)
+{
+        if(num>num2)
+        {
+            return num;
+        }
+        else
+        {
+            return num2;
+        }
+}
+
+int Max3(int num, int num2,int num3)
+{
+     if(num>num2 && num>num3)
+        {
+            return num;
+        }
+    else if(num2>num && num2>num3)
+        {
+            return num2;
+        }
+    else
+        {
+            return num3;    
+        }
+}
+
+int Max10(std :: array<int, 10> nums)
+{
+    int highest=0;
+    for(int ix=0;ix<10;ix++)
+    {
+        if(nums[ix]>highest)
+        {
+            highest=nums[ix];
+        }
+    }
+    return highest;
+}

383774526d4e9e2216558f591ab043adee62c4c0
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 11:47:26 -0700

    mod5

--- hafb-module3/LA3-4/src/distance.cpp
+++ hafb-module3/LA3-4/src/distance.cpp
@@ -4,3 +4,86 @@
 #include <iostream>
 using namespace std;
 #include "distance.h"
+
+ void Distance:: ShowDist() const
+ {
+    std:: cout << feet() << "\'-" << inches() << "\"" <<std:: endl;
+ }
+/**
+ * @brief Pluse + Operator for distance objects
+ * 
+ * @param d2 Secpmd Dostamce pbkect
+ * @return Distance operator + : si,e pf se;f +d2 objects
+ */
+ Distance Distance :: operator +(Distance d2) const
+ {
+   int feet=feet_+d2.feet();
+   float inch=inches_+d2.inches();
+   while(inch>12.0)
+   {
+      feet++;
+      inch=inch-12;
+   }
+    return Distance(feet,inch);
+ }
+  Distance Distance :: operator -(Distance d2) const
+ {
+   int feet=feet_-d2.feet();
+   float inch=inches_-d2.inches();
+   while(inch<0)
+   {
+      inch=inch+12.0;
+      feet--;
+   }
+  
+    return Distance(feet,inch);
+ }
+
+   bool Distance :: operator <(Distance d2) const
+ {
+     float inch1=feet_*12+inches_;
+     float inch2=d2.feet()*12+d2.inches();
+    if(inch1<inch2)
+    {
+        return true;
+    }
+    return false;
+ }
+
+   bool Distance :: operator ==(Distance d2) const
+ {
+     float inch1=feet_*12+inches_;
+     float inch2=d2.feet()*12+d2.inches();
+    if(inch1==inch2)
+    {
+        return true;
+    }
+    return false;
+ }
+ std:: ostream &operator <<(std::ostream &os, const Distance &distance)
+ {
+
+    //Do not include a \n or endl character
+    os<< "Feet: "<< distance.feet_<<" Inches: "<<distance.inches_;
+    return os;
+ }
+ void Distance:: update_distance(int ft, float in)
+{
+   set_feet(ft);
+   set_inches(in);
+}
+
+  Distance Distance :: operator =(Distance d2)
+ {
+     std:: cout<<"New Assignement"<< endl;
+    int feet=d2.feet();
+    float inches=d2.inches();
+    return Distance(feet,inches);
+ }
+
+ Distance:: Distance(const Distance& dist)
+ {
+     std:: cout<<"Copy constructor invoked" << std::endl;
+     feet_=dist.feet_;
+     inches_=dist.inches_;
+ }
---
+++ hafb-module3/LA3-4/src/distance.h
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#pragma
+
+class Distance
+{
+    private:
+        int feet_;
+        float inches_;
+    public:
+
+
+        //Constructor
+        Distance():feet_(0),inches_(0){}
+        Distance(int feet,float inches):feet_(feet),inches_(inches){}
+        ~Distance(){}
+        //Gets&Setters
+        int feet() const{return feet_;}
+        void set_feet(int feet) {feet_=feet;}
+        
+        float inches() const{return inches_;}
+        void set_inches(int inches) {inches_=inches;}
+        // Other Methods
+        void ShowDist() const;
+
+        Distance operator +(Distance d2) const;
+        friend std:: ostream &operator <<(std::ostream &os, const Distance &distance);
+        Distance operator -(Distance d2) const;
+        void update_distance(int ft, float in);
+        bool operator <(Distance d2) const;
+        bool operator ==(Distance d2) const;
+        Distance operator =(Distance d2);
+        Distance(const Distance& dist);
+
+};
+
+// distance::distance()
+// {
+// }
+
+// distance::~distance()
+// {
+// }
